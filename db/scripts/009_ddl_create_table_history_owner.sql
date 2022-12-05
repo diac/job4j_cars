7abcf7fddb25122004cfae078237d2195536dcaa
diac <nikolai.gladkikh@gmail.com>
2022-12-05 13:14:45 +0300

    Модели и связи. Машины и владельцы [#4744]: Исправить PRIMARY KEY в таблице history_owner

--- db/scripts/009_ddl_create_table_history_owner.sql
+++ db/scripts/009_ddl_create_table_history_owner.sql
@@ -1,7 +1,8 @@
 CREATE TABLE history_owner (
+    id SERIAL PRIMARY KEY,
     car_id INTEGER NOT NULL REFERENCES car(id),
     driver_id INTEGER NOT NULL REFERENCES driver(id),
     startAt DATE NOT NULL,
     endAt DATE,
-    PRIMARY KEY(car_id, driver_id)
+    UNIQUE(car_id, driver_id, startAt, endAt)
 )
