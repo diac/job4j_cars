d4e6bc83772ff1c41cb475944f39ba2f2c9b4d7c
diac <nikolai.gladkikh@gmail.com>
2022-12-06 08:46:07 +0300

    Модели и связи. Машины и владельцы [#4744 #367760]: У таблицы history_owner в качестве PK должен быть полный набор полей. Удалить модель HistoryOwner.java

--- db/scripts/009_ddl_create_table_history_owner.sql
+++ db/scripts/009_ddl_create_table_history_owner.sql
@@ -1,8 +1,7 @@
 CREATE TABLE history_owner (
-    id SERIAL PRIMARY KEY,
     car_id INTEGER NOT NULL REFERENCES car(id),
     driver_id INTEGER NOT NULL REFERENCES driver(id),
     startAt DATE NOT NULL,
     endAt DATE,
-    UNIQUE(car_id, driver_id, startAt, endAt)
+    PRIMARY KEY(car_id, driver_id, startAt, endAt)
 )
