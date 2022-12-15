8422a136d44842a15235c76f0e44757fcd4fd086
diac <nikolai.gladkikh@gmail.com>
2022-12-15 16:21:21 +0300

    Добавить утилитный класс Cars

---
+++ src/main/java/ru/job4j/cars/util/Cars.java
@@ -0,0 +1,25 @@
+package ru.job4j.cars.util;
+
+import ru.job4j.cars.model.Car;
+
+import java.util.function.Function;
+
+public final class Cars {
+
+    private Cars() {
+
+    }
+
+    public static Function<Car, String> displayNameHelper() {
+        return car -> String.format(
+                "%s %s, %s, %s (%s, %s, %d л/с)",
+                car.getBrand().getName(),
+                car.getModelName(),
+                car.getExteriorColor().getName(),
+                car.getBodyStyle().getName(),
+                car.getEngineType().getName(),
+                car.getEngineVolume().getName(),
+                car.getHorsepower()
+        );
+    }
+}
