8b31018faa6994fb9d5847e83933bc64a2e726a3
diac <nikolai.gladkikh@gmail.com>
2022-12-17 12:57:56 +0300

    Добавить модель PostSearchResult

---
+++ src/main/java/ru/job4j/cars/model/PostSearchResult.java
@@ -0,0 +1,57 @@
+package ru.job4j.cars.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.job4j.cars.enumeration.SteeringWheelSide;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "post_search")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostSearchResult {
+
+    @Column(name = "post_id")
+    private int postId;
+
+    @Column(name = "car_id")
+    private int carId;
+
+    @Column(name = "body_style_id")
+    private int bodyStyleId;
+
+    @Column(name = "exterior_color_id")
+    private int exteriorColorId;
+
+    @Column(name = "transmission_type_id")
+    private int transmissionTypeId;
+
+    @Column(name = "drivetrain_id")
+    private int drivetrainId;
+
+    @Column(name = "steering_wheel_side")
+    private SteeringWheelSide steeringWheelSide;
+
+    @Column(name = "model_name")
+    private String modelName;
+
+    @Column(name = "engine_type_id")
+    private int engineTypeId;
+
+    @Column(name = "brand_id")
+    private int brandId;
+
+    @Column(name = "horsepower")
+    private int horsepower;
+
+    @Column(name = "production_year")
+    private int productionYear;
+
+    @Column(name = "engine_volume")
+    private int engineVolume;
+}
