586796b7a0baf9ac0558523b0a471a95c2165afb
diac <nikolai.gladkikh@gmail.com>
2022-12-17 11:56:47 +0300

    В PostSearchParams заменить тип полей на Integer

--- src/main/java/ru/job4j/cars/model/PostSearchParams.java
+++ src/main/java/ru/job4j/cars/model/PostSearchParams.java
@@ -10,18 +10,18 @@ import ru.job4j.cars.enumeration.SteeringWheelSide;
 @NoArgsConstructor
 public class PostSearchParams {
 
-    private int carBrandId;
+    private Integer carBrandId;
     private String carModel;
-    private int carBodyStyleId;
-    private int carExteriorColorId;
-    private int carTransmissionTypeId;
-    private int carDrivetrainId;
+    private Integer carBodyStyleId;
+    private Integer carExteriorColorId;
+    private Integer carTransmissionTypeId;
+    private Integer carDrivetrainId;
     private SteeringWheelSide carSteeringWheelSide;
-    private int carEngineTypeId;
-    private int carEngineVolumeMin;
-    private int carEngineVolumeMax;
-    private int carHorsepowerMin;
-    private int carHorsepowerMax;
-    private int carProductionYearMin;
-    private int carProductionYearMax;
+    private Integer carEngineTypeId;
+    private Integer carEngineVolumeMin;
+    private Integer carEngineVolumeMax;
+    private Integer carHorsepowerMin;
+    private Integer carHorsepowerMax;
+    private Integer carProductionYearMin;
+    private Integer carProductionYearMax;
 }
