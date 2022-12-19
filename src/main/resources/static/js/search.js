093d2597c6327e2acf6969bcad6ac84679f433a1
diac <nikolai.gladkikh@gmail.com>
2022-12-19 13:31:54 +0300

    На странице поиска добавить возможность сворачивать и разворачивать секцию с дополнительными параметрами

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -1,6 +1,6 @@
 (function($) {
     $(document).ready(function() {
-        $("#searchForm").on("submit", function(event ) {
+        $("#searchForm").on("submit", function(event) {
             event.preventDefault();
             var searchUrlParams = $("#searchForm :input")
                 .filter(function(index, element) {
@@ -8,6 +8,20 @@
                 })
                 .serialize();
             location.href = "/?" + searchUrlParams;
+        });
+
+        $("#searchForm #expandControl").click(function() {
+            if ($("#searchForm .expandable").hasClass("collapsed")) {
+                $("#searchForm .expandable").slideDown(function() {
+                    $("#searchForm .expandable").removeClass("collapsed");
+                    $("#expandControl .caption").html("Скрыть");
+                });
+            } else {
+                $("#searchForm .expandable").slideUp(function() {
+                    $("#searchForm .expandable").addClass("collapsed");
+                    $("#expandControl .caption").html("Показать");
+                });
+            }
         });
     });
 }(jQuery));
